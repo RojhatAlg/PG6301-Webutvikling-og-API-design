11fb8d2dcdcf0a540f74086bceef8e09b1c5f471
RojhatAlg <roal005@student.kristiania.no>
2024-08-13 14:08:46 +0200

    basis of code

---
+++ client/src/Components/AdminDashboard/AdminDashboard.jsx
@@ -0,0 +1,6 @@
+//Admin får en velkomst
+//Admin kan velge å se nyhetsartikler, deretter velge å se detaljer per nyhetsartikkel (overskrift, navn, bilde)
+//Admin kan velge å se sin egen profilside med bruker info + artikler brukeren har opprettet
+//Admin kan velge å opprette en artikkel (bruker skal forhindres fra å sende en nyhetsartikkel som mangler kategori, tittel, tekst)
+//bAdmin skal kunne redigere en artikkel de selv har publisert
+//Admin skal kunne slette en artikkel de selv har publisert
---
+++ client/src/Components/UserDashboard/UserDashboard.jsx
@@ -0,0 +1,4 @@
+//bruker får en velkomst
+//bruker kan se sin profilside med profil info
+//bruker kan velge å se nyhetsartikler
+//brukere kan lese nyhetsartikklene ved å trykke på "les mer" og deretter "vis detaljer" for å se titel, navn og bilde på den som publiserte artikkelen
